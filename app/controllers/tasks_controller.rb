b405513eb3466d9a43db34313ea568780ac615b7
EC2 Default User <ec2-user@ip-172-31-15-138.us-east-2.compute.internal>
2018-04-06 12:05:11 +0000

    CRUD tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,3 +47,10 @@ class TasksController < ApplicationController
     redirect_to tasks_url
   end
   end
+  
+    private
+
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:task)
+  end
