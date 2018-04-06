a9f0652dd86265ce8c5bbc249b02b6de17afff78
EC2 Default User <ec2-user@ip-172-31-15-138.us-east-2.compute.internal>
2018-04-06 14:01:22 +0000

    add status

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -48,9 +48,13 @@ class TasksController < ApplicationController
   end
   
     private
+    
+    def set_task
+    @task = Task.find(params[:id])
+    end
 
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end
